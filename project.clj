5f6f5b355a4ef7e237dfd27f5cb2d59df4c97eac
Jack You <Jack.you@hotmail.com>
2018-03-13 22:29:05 -0400

    updated project.clj with paths, parser complete for stack instructions

--- project.clj
+++ project.clj
@@ -3,7 +3,10 @@
   :url "https://github.com/jacksyou/clj-whitespace"
   :license {:name "MIT Public License"
             :url "https://opensource.org/licenses/MIT"}
-  :dependencies [[org.clojure/clojure "1.8.0"]]
+  :dependencies [[org.clojure/clojure "1.8.0"]
+                 [org.clojure/core.match "0.3.0-alpha5"]
+                 ]
   :main ^:skip-aot clj-whitespace.core
+  :java-source-paths ["src/" "test/"]
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
--- src/clj_whitespace/core.clj
+++ src/clj_whitespace/core.clj
@@ -1,7 +1,11 @@
 (ns clj-whitespace.core
+  (:require [clj-whitespace.parser :as parser])
   (:gen-class))
 
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
-  (println "Hello, World!"))
+  (def test1 '(" " " " " " "\t" " " "\n" " " "\n" "\t" "\n" "\n" "\n"))
+  (prn test1)
+  (println (parser/parse test1)))
+
