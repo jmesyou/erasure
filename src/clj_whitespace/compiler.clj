96fc26f8380562ef629e5899386db5d00d96c9bc
Jack You <jack.you@hotmail.com>
2018-04-03 18:06:13 -0400

    optimized compiler and runtime with tail call optimization

--- src/clj_whitespace/compiler.clj
+++ src/clj_whitespace/compiler.clj
@@ -3,12 +3,17 @@
     (:require [clj-whitespace.parser :as parser])
     (:gen-class))
 
-(defn compile-program [cmds program labels]
-    (match [cmds]
-        [([([:label l] :as x) & xs] :seq)] (if (contains? labels l) 
-                                        ((throw (Exception. "label already present in global table")))
-                                        (recur xs (conj program x) (conj labels {l xs})))
-        [([x & xs] :seq)] (recur xs (conj program x) labels)
-        :else [program labels]
-        ))
+(defn compile-program [cmds prgm labels]
+    (if (empty? cmds)
+        [prgm labels]
+        (let [[n-cmds n-prgm n-labels] 
+              (match [cmds]
+                [([[:push a] :pop & xs] :seq)] [xs prgm labels]
+                [([[:push a] [:push b] :swap & xs] :seq)] [xs (concat prgm [[:push b] [:push a]]) labels]
+                [([([:label l] :as x) & xs] :seq)] (if (contains? labels l) 
+                                                       (throw (Exception. "label already present in global table"))
+                                                       [xs (conj prgm x) (conj labels {l xs})])
+                [([x & xs] :seq)] [xs (conj prgm x) labels]
+                [([:end] :seq)] [(list) (conj prgm :end) labels])]
+             (recur n-cmds n-prgm n-labels))))
 
--- src/clj_whitespace/runtime.clj
+++ src/clj_whitespace/runtime.clj
@@ -1,22 +1,26 @@
 (ns clj-whitespace.runtime
     (:require [clojure.core.match :refer [match]])
+    (:import [jline.console ConsoleReader])
     (:gen-class))
-
+    
 (defn routine [prgm stack table labels call-stack]
     (if (empty? prgm)
         (do ())
-        (match [prgm]
-            [([[:push v] & xs] :seq)] (routine xs (conj stack v) table labels call-stack)
-            [([:dup & xs] :seq)] (let [x (peek stack)] (routine xs (conj stack x) table labels call-stack))
-            [([:swap & xs] :seq)] (let [a (first stack) b (second stack)] (routine xs (concat [b a] (nnext stack)) table labels call-stack))
-            [([:pop & xs] :seq)] (routine xs (pop stack) table labels call-stack) 
-            [([:add & xs] :seq)] (let [a (first stack) b (second stack)] (routine xs (conj (nnext stack) (+ b a)) table labels call-stack))
-            [([:sub & xs] :seq)] (let [a (first stack) b (second stack)] (routine xs (conj (nnext stack) (- b a)) table labels call-stack))
-            [([:mul & xs] :seq)] (let [a (first stack) b (second stack)] (routine xs (conj (nnext stack) (* b a)) table labels call-stack))
-            [([:div & xs] :seq)] (let [a (first stack) b (second stack)] (routine xs (conj (nnext stack) (/ b a)) table labels call-stack))
-            [([:mod & xs] :seq)] (let [a (first stack) b (second stack)] (routine xs (conj (nnext stack) (mod b a)) table labels call-stack))
-            [([:print-char & xs] :seq)] (do (print (char (first stack))) (routine xs stack table labels call-stack))
-            [([:print-int & xs] :seq)] (do (print (first stack)) (routine xs stack table labels call-stack))
-            :else (throw (Exception. "[runtime/routine] unexpected or malformed op!")))))
+        (let [[n-prgm n-stack n-table n-labels n-call-stack] 
+             (match [prgm]
+             [([[:push v] & xs] :seq)] [xs (conj stack v) table labels call-stack]
+             [([:dup & xs] :seq)] (let [x (peek stack)] [xs (conj stack x) table labels call-stack])
+             [([:swap & xs] :seq)] (let [a (first stack) b (second stack)] [xs (concat [b a] (nnext stack)) table labels call-stack])
+             [([:pop & xs] :seq)] [xs (pop stack) table labels call-stack] 
+             [([:add & xs] :seq)] (let [a (first stack) b (second stack)] [xs (conj (nnext stack) (+ b a)) table labels call-stack])
+             [([:sub & xs] :seq)] (let [a (first stack) b (second stack)] [xs (conj (nnext stack) (- b a)) table labels call-stack])
+             [([:mul & xs] :seq)] (let [a (first stack) b (second stack)] [xs (conj (nnext stack) (* b a)) table labels call-stack])
+             [([:div & xs] :seq)] (let [a (first stack) b (second stack)] [xs (conj (nnext stack) (/ b a)) table labels call-stack])
+             [([:mod & xs] :seq)] (let [a (first stack) b (second stack)] [xs (conj (nnext stack) (mod b a)) table labels call-stack])
+             [([:print-char & xs] :seq)] (do (print (char (first stack))) [xs stack table labels call-stack])
+             [([:print-int & xs] :seq)] (do (print (first stack)) [xs stack table labels call-stack])
+             [([(:or :read-char :read-int) & xs] :seq)] (let [keyint (Integer/parseInt (read-line)) addr (first stack)] [xs stack (conj table {addr keyint}) labels call-stack])
+             :else (throw (Exception. "[runtime/routine] unexpected or malformed op!")))]
+             (recur n-prgm n-stack n-table n-labels n-call-stack))))
 
 (defn exec [prgm labels] (routine prgm '() {} labels nil))
